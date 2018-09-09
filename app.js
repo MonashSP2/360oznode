fa74f89449194d7a571fd7b68b7c7f4ca7053848
Shuhan <sche0022@student.manosh.edu>
2018-09-09 23:26:23 +1000

    node server 1.3 proxy access right

--- app.js
+++ app.js
@@ -63,6 +63,24 @@ app.use('/clinic',clinicRoutes);
 app.use('/community',communityRoutes);
 app.use('/store',storeRoutes);
 
+//导入proxy
+const proxy = require('http-proxy-middleware');
+
+//context可以是单个字符串，也可以是多个字符串数组，分别对应你需要代理的api,星号（*）表示匹配当前路径下面的所有api
+const context = [`/`, `/*`]
+
+//options可选的配置参数请自行看readme.md文档，通常只需要配置target，也就是你的api所属的域名。
+const options = {
+    target: 'http://35.189.58.222',
+    changeOrigin: true
+}
+
+//将options对象用proxy封装起来，作为参数传递
+const apiProxy = proxy(options)
+
+//现在你只需要执行这一行代码，当你访问需要跨域的api资源时，就可以成功访问到了。
+app.use(context, apiProxy)
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const error = new Error('Not found');
