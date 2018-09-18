b5e7d08240149a703f5afeec18ba0aab94a05246
Shuhan <sche0022@student.manosh.edu>
2018-09-18 22:27:16 +1000

    node server 1.9 Add 5 parameters in the onedaychallenge url api

--- routes/oneDayChallenge.js
+++ routes/oneDayChallenge.js
@@ -228,6 +228,7 @@ router.get('/:A/:B/:C/:lat/:lng', (req, res, next) => {
 
 });
 
+//four parameters
 router.get('/:A/:B/:C/:D/:lat/:lng', (req, res, next) => {
 
     const A = req.params.A;
@@ -379,6 +380,203 @@ router.get('/:A/:B/:C/:D/:lat/:lng', (req, res, next) => {
 
 
                                     res.status(200).end(JSON.stringify(aResutlt, undefined, 2))
+                                });
+                        });
+
+                });
+
+
+        });
+
+});
+
+//five parameters
+router.get('/:A/:B/:C/:D/:E/:lat/:lng', (req, res, next) => {
+
+    const A = req.params.A;
+    const B = req.params.B;
+    const C = req.params.C;
+    const D = req.params.D;
+    const E = req.params.E;
+    let lat = Number(req.params.lat);
+    let lng = Number(req.params.lng);
+    // lat = -37.884;
+    // lng=145.0266;
+
+    const urlA = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='
+        + lat + ',' + lng + '&radius=1500&keyword=' + A + '&key='+KEY;
+    const urlB = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='
+        + lat + ',' + lng + '&radius=1500&keyword=' + B + '&key='+KEY;
+    const urlC = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='
+        + lat + ',' + lng + '&radius=1500&keyword=' + C + '&key='+KEY;
+    const urlD = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='
+        + lat + ',' + lng + '&radius=1500&keyword=' + D + '&key='+KEY;
+    const urlE = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='
+        + lat + ',' + lng + '&radius=1500&keyword=' + E + '&key='+KEY;
+    // const fetch = require('node-fetch');
+
+    let aArray = [];
+    let bArray = [];
+    let cArray = [];
+    let dArray = [];
+    let eArray = [];
+
+    let aResutlt = [];
+
+    fetch(urlA)
+        .then(res => res.json())
+        .then(json => {
+            let resultArray = json["results"];
+            for (let i = 0; i < resultArray.length; i++) {
+                let aObject = {
+                    "id": resultArray[i]["id"],
+                    "name": resultArray[i]["name"],
+                    "latitude": resultArray[i]["geometry"]["location"]["lat"],
+                    "longitude": resultArray[i]["geometry"]["location"]["lng"]
+                };
+                aArray.push(aObject)
+            }
+            fetch(urlB)
+                .then(res => res.json())
+                .then(json => {
+
+                    let resultArray = json["results"];
+                    for (let i = 0; i < resultArray.length; i++) {
+                        let aObject = {
+                            "id": resultArray[i]["id"],
+                            "name": resultArray[i]["name"],
+                            "latitude": resultArray[i]["geometry"]["location"]["lat"],
+                            "longitude": resultArray[i]["geometry"]["location"]["lng"]
+                        };
+                        bArray.push(aObject)
+
+                    }
+                    fetch(urlC)
+                        .then(res => res.json())
+                        .then(json => {
+
+                            let resultArray = json["results"];
+                            for (let i = 0; i < resultArray.length; i++) {
+                                let aObject = {
+                                    "id": resultArray[i]["id"],
+                                    "name": resultArray[i]["name"],
+                                    "latitude": resultArray[i]["geometry"]["location"]["lat"],
+                                    "longitude": resultArray[i]["geometry"]["location"]["lng"]
+                                };
+                                cArray.push(aObject)
+                            }
+                            fetch(urlD)
+                                .then(res => res.json())
+                                .then(json => {
+
+                                    let resultArray = json["results"];
+                                    for (let i = 0; i < resultArray.length; i++) {
+                                        let aObject = {
+                                            "id": resultArray[i]["id"],
+                                            "name": resultArray[i]["name"],
+                                            "latitude": resultArray[i]["geometry"]["location"]["lat"],
+                                            "longitude": resultArray[i]["geometry"]["location"]["lng"]
+                                        };
+                                        dArray.push(aObject)
+                                    }
+
+                                    fetch(urlE)
+                                        .then(res => res.json())
+                                        .then(json => {
+
+                                            let resultArray = json["results"];
+                                            for (let i = 0; i < resultArray.length; i++) {
+                                                let aObject = {
+                                                    "id": resultArray[i]["id"],
+                                                    "name": resultArray[i]["name"],
+                                                    "latitude": resultArray[i]["geometry"]["location"]["lat"],
+                                                    "longitude": resultArray[i]["geometry"]["location"]["lng"]
+                                                };
+                                                eArray.push(aObject)
+                                            }
+
+
+                                            let temp1 = '';
+                                            let temp2 = '';
+                                            let temp0 = 9999999;
+
+                                            for (let j = 0; j < aArray.length; j++) {
+                                                let distance = Math.abs(aArray[j]["longitude"] - lng)
+                                                    + Math.abs(aArray[j]["latitude"] - lat);
+                                                if (distance < temp0) {
+                                                    temp0 = distance;
+                                                    // temp1 = aArray[i];
+                                                    temp2 = aArray[j];
+                                                }
+                                            }
+                                            if (A !== "a") {
+                                                aResutlt.push(temp2);
+                                            }
+                                            // ltemp1 = '';
+                                            temp2 = '';
+                                            temp0 = 9999999;
+
+                                            for (let j = 0; j < bArray.length; j++) {
+                                                let distance = Math.abs(bArray[j]["longitude"] - lng)
+                                                    + Math.abs(bArray[j]["latitude"] - lat);
+                                                if (distance < temp0) {
+                                                    temp0 = distance;
+                                                    // temp1 = aArray[i];
+                                                    temp2 = bArray[j];
+                                                }
+                                            }
+                                            if (B !== "a") {
+                                                aResutlt.push(temp2);
+                                            }
+                                            temp2 = '';
+                                            temp0 = 9999999;
+
+                                            for (let j = 0; j < cArray.length; j++) {
+                                                let distance = Math.abs(cArray[j]["longitude"] - lng)
+                                                    + Math.abs(cArray[j]["latitude"] - lat);
+                                                if (distance < temp0) {
+                                                    temp0 = distance;
+                                                    // temp1 = aArray[i];
+                                                    temp2 = cArray[j];
+                                                }
+                                            }
+                                            if (C !== "a") {
+                                                aResutlt.push(temp2);
+                                            }
+                                            temp2 = '';
+                                            temp0 = 9999999;
+
+                                            for (let j = 0; j < dArray.length; j++) {
+                                                let distance = Math.abs(dArray[j]["longitude"] - lng)
+                                                    + Math.abs(dArray[j]["latitude"] - lat);
+                                                if (distance < temp0) {
+                                                    temp0 = distance;
+                                                    // temp1 = aArray[i];
+                                                    temp2 = dArray[j];
+                                                }
+                                            }
+                                            if (D !== "a") {
+                                                aResutlt.push(temp2);
+                                            }
+                                            temp2 = '';
+                                            temp0 = 9999999;
+
+                                            for (let j = 0; j < eArray.length; j++) {
+                                                let distance = Math.abs(eArray[j]["longitude"] - lng)
+                                                    + Math.abs(eArray[j]["latitude"] - lat);
+                                                if (distance < temp0) {
+                                                    temp0 = distance;
+                                                    // temp1 = aArray[i];
+                                                    temp2 = eArray[j];
+                                                }
+                                            }
+                                            if (E !== "a") {
+                                                aResutlt.push(temp2);
+                                            }
+
+
+                                            res.status(200).end(JSON.stringify(aResutlt, undefined, 2))
+                                        });
                                 });
                         });
 
